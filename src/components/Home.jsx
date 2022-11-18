16b38f67dc99d4e38177893d3bf83ad25040d4a4
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-11-18 12:35:14 +0100

    iter1 done

---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+function Home() {
+  return (
+    <div className="home">
+        <div>
+          <img src="../assets/beers.png" alt="bar" />
+          <h2>All Beers</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis urna sed libero porta commodo.</p>
+        </div>
+        <div>
+          <img src="../assets/random-beer.png" alt="beer draft" />
+          <h2>Random Beer</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis urna sed libero porta commodo.</p>
+        </div>
+        <div>
+          <img src="../assets/random-beer.png" alt="beer glass and bottle" />
+          <h2>New Beer</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis urna sed libero porta commodo.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Home
